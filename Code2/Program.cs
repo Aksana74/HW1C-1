e2f606e82c05800bb3baca5bdfb733fac0ff232f
Aksana74 <detkovao185@gmail.com>
2023-04-23 17:59:00 +0300

    Сode2

---
+++ Code2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Максимальное из трех чисел
+Console.Write("Первое число (a:)");
+int i = int.Parse(Console.ReadLine());
+
+Console.Write("Второе число (b:)");
+int k = int.Parse(Console.ReadLine());
+
+Console.Write("Третье число (с:)");
+int l = int.Parse(Console.ReadLine());
+
+int max =i;
+
+if(i>max) max = i ;
+if(k>max) max = k;
+if(l>max) max = l;
+{
+Console.Write("max=");    
+Console.WriteLine (max);
+}
