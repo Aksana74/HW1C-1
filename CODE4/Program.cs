27b81a710dd06e0e9565f6395f106d5f5a93e2e1
Aksana74 <detkovao185@gmail.com>
2023-04-23 18:08:30 +0300

    CODE4

---
+++ CODE4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Четные от 1 до N
+System.Console.WriteLine("Введите число N:");
+int s = int.Parse(Console.ReadLine());
+int i =1;
+while (i<=s)
+{
+    if (i%2==0)
+    {
+Console.Write(i+"");
+i++;
+    }
+i++;
+ }
+
