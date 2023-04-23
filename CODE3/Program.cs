b3220714587d37dcff84ce83356df615fb99c850
Aksana74 <detkovao185@gmail.com>
2023-04-23 18:07:36 +0300

    CODE3

---
+++ CODE3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Четные числа.
+System.Console.WriteLine("Введите число N:");
+int s = int.Parse(Console.ReadLine());
+if (s>= - 10 && s <= 10)
+{
+System.Console.WriteLine(s%2==0);
+}
+else{
+    System.Console.WriteLine("Это нечетное число"); 
+    
+}
