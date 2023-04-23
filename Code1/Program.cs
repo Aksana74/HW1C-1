f7546e9d7150ce26151f47e46e405dcac40af8bc
Aksana74 <detkovao185@gmail.com>
2023-04-23 17:48:30 +0300

    Code1

---
+++ Code1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Сравнение двух чисел
+Console.WriteLine("Введите первое число(a:)");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Второе число (b:)");
+int b = int.Parse(Console.ReadLine());
+if (a>b){
+Console.WriteLine(a+"= max");
+    }
+    else{ 
+Console.WriteLine(a+"= min");
+ }
+     if (b>a){
+Console.WriteLine(b+"= max");
+}
+     else{ 
+Console.WriteLine(b+"= min");
+ }
+ System.Console.WriteLine("Упс");
+
